e5d4dccf3e4213dc1f1c46c264af9a021e8915fc
Odakit <tanlalan123@gmail.com>
2019-11-09 21:28:13 +0700

    Update Scene Merge

--- sources/TheLionKing/Assets/Scripts/MergeUI/GameMaster.cs
+++ sources/TheLionKing/Assets/Scripts/MergeUI/GameMaster.cs
@@ -7,15 +7,21 @@ public class GameMaster : MonoBehaviour
     // Start is called before the first frame update
     public bool isMerge = false;
     public int CountItem = 6;
+    public GameObject ReturnHomeFlower;
     void Start()
     {
         gameObject.SetActive(true);
+        ReturnHomeFlower.SetActive(false);
     }
 
+    
     // Update is called once per frame
     void Update()
     {
-        
+        if(CountItem==0)
+        {
+            ReturnHomeFlower.SetActive(true);
+        }
     }
 
     public void HideMergeCanvas()
