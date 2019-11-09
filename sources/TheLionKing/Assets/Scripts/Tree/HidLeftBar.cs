8f3a83e126b1d3c44fe4bc20bf748b9bdb81a844
Odakit <tanlalan123@gmail.com>
2019-11-09 10:38:43 +0700

    Hide bar , Light animation

---
+++ sources/TheLionKing/Assets/Scripts/Tree/HidLeftBar.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HidLeftBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool isHide = false;
+    GameObject HideButton;
+    int InOut = 0;
+    float minHide = -60f;
+    float maxHide = 0;
+    void Start()
+    {
+        HideButton = GameObject.FindGameObjectWithTag("TreeHideButton");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(gameObject.transform.localPosition.x);
+        if(isHide==true)
+        {
+            if(gameObject.transform.localPosition.x>minHide && InOut%2==0)
+            {
+                float a = gameObject.transform.localPosition.x;
+                a -= 2f;
+                gameObject.transform.localPosition = new Vector2(a, gameObject.transform.localPosition.y);
+            }
+            else if(gameObject.transform.localPosition.x < maxHide && InOut % 2 != 0)
+            {
+                float a = gameObject.transform.localPosition.x;
+                a += 2f;
+                gameObject.transform.localPosition = new Vector2(a, gameObject.transform.localPosition.y);
+            }
+            else
+            {
+                isHide = false;
+            }
+        }
+    }
+
+    public void ButtonHideClick()
+    {
+        float a = HideButton.transform.localScale.x;
+        a *= -1;
+        HideButton.transform.localScale = new Vector2(a, HideButton.transform.localScale.y);
+        InOut++;
+        isHide = true;
+    }
+
+
+
+}
