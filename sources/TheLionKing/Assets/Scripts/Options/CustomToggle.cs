618c0b6d73a235a663101da2dd73731fbf76f69a
Nguyễn Văn Phước <phuocnv2@vng.com.vn>
2019-11-09 16:55:28 +0700

    add dialog options

---
+++ sources/TheLionKing/Assets/Scripts/Options/CustomToggle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CustomToggle: MonoBehaviour
+{
+    public Image targetGraphic;
+    public Image target;
+
+    public bool IsOn = false;
+    
+    public void OnTargetClick()
+    {
+        IsOn = !IsOn;
+        Vector3 newPos = target.transform.position;
+        if (IsOn)
+        {
+            newPos.x += target.rectTransform.sizeDelta.x / 2;
+        } else
+        {
+            newPos.x -= target.rectTransform.sizeDelta.x / 2;
+        }
+        target.transform.position = newPos;
+    }
+}
